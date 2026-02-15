5fe723c77b7c642febd2ed31be5c649b90d45717
chiaracariolato <chiaracariolato@gmail.com>
2026-02-15 17:28:15 +0100

    feat: added style and data display

--- js/script.js
+++ js/script.js
@@ -1,17 +1,24 @@
-const sendButton = document.querySelector("button");
+let prezzo = document.getElementById("price");
+let form = document.querySelector("form");
+let inputEta = document.getElementById("finalAge");
+let inputKm = document.getElementById("finalKm");
 
-sendButton.addEventListener("click", 
-function() {
-    let eta = document.getElementById("age").value;
-    let km = document.getElementById("km").value;
+
+form.addEventListener("submit", 
+function(e) {
+    e.preventDefault();
+    let eta = document.getElementById("age").value.trim();
+    let km = document.getElementById("km").value.trim();
+    inputEta.innerText = eta + " anni";
+    inputKm.innerText = km + " km";
+
     if(eta < 18) {
-        let prezzo = ((km * 0.21) * 0.80);
-        console.log("Il prezzo del biglietto è " + prezzo.toFixed(2) + "€");
+        prezzo.innerText = ((km * 0.21) * 0.80).toFixed(2) + "€";
     } else if(eta >= 65) {
-        let prezzo = ((km * 0.21) * 0.60);
-        console.log("Il prezzo del biglietto è " + prezzo.toFixed(2) + "€");
+        prezzo.innerText = ((km * 0.21) * 0.60).toFixed(2) + "€";
     } else {
-        let prezzo = (km * 0.21);
-        console.log("Il prezzo del biglietto è " + prezzo.toFixed(2) + "€");
+        prezzo.innerText = (km * 0.21).toFixed(2) + "€";
     }
+    
+    form.reset()
 })
